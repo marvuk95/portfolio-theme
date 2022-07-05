38f899aa3b4d74b271bc10c2cd241325f9f2ded9
Marko Vukmirovic <mvsrb55@gmail.com>
2022-07-05 04:46:38 -0700

    Added wave animations and project cards styles on the front page

--- js/morph.js
+++ js/morph.js
@@ -5,9 +5,62 @@ const tween = KUTE.fromTo(
     { repeat: 999, duration: 3000, yoyo: true }
 ).start();
 
+
+//Home section animation
+const wave = KUTE.fromTo(
+    '#water1',
+    { path: '#water1' },
+    { path: '#water2' },
+    { repeat: 999, duration: 3000, yoyo: true }
+).start();
+
+const wave2 = KUTE.fromTo(
+    '#water3',
+    { path: '#water3' },
+    { path: '#water4' },
+    { repeat: 999, duration: 3000, yoyo: true }
+).start();
+
+const wave3 = KUTE.fromTo(
+    '#water5',
+    { path: '#water5' },
+    { path: '#water6' },
+    { repeat: 999, duration: 3000, yoyo: true }
+).start();
+
+const wave4 = KUTE.fromTo(
+    '#water7',
+    { path: '#water7' },
+    { path: '#water8' },
+    { repeat: 999, duration: 3000, yoyo: true }
+).start();
+
+
+//Contact section animation
 const tweens = KUTE.fromTo(
     '#wave1',
     { path: '#wave1' },
     { path: '#wave2' },
+    { repeat: 999, duration: 3000, yoyo: true }
+).start();
+
+const tweens2 = KUTE.fromTo(
+    '#wave3',
+    { path: '#wave3' },
+    { path: '#wave4' },
+    { repeat: 999, duration: 3000, yoyo: true }
+).start();
+
+const tweens3 = KUTE.fromTo(
+    '#wave5',
+    { path: '#wave5' },
+    { path: '#wave6' },
+    { repeat: 999, duration: 3000, yoyo: true }
+).start();
+
+const tweens4 = KUTE.fromTo(
+    '#wave7',
+    { path: '#wave7' },
+    { path: '#wave8' },
     { repeat: 999, duration: 3000, yoyo: true }
 ).start();
